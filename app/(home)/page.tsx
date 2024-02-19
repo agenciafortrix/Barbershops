b8fda95072413489392c51592c901d0ec3d1de1b
Lusca-rcg <lusca.rcg@gmail.com>
2024-02-19 20:58:13 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,18 @@
-import Image from "next/image";
+import { format } from "date-fns";
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale";
 
 export default function Home(){
   return (
     <div>
       <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Ola, Pedro!</h2>
+        <p className="captalize text-sm">{format(new Date(), "EEEE',' d 'de' LLLL  y ",{
+          locale: ptBR,
+        })}</p>
+      </div>
     </div>
   )
 }
