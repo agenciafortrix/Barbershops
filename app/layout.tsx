7cb2e9a9750a496ed0d4c58fcadfcc994e221800
Lusca-rcg <lusca.rcg@gmail.com>
2024-02-21 22:48:12 -0300

    feat: add AuthProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "./_components/footer";
+import AuthProvider from "./_providers/auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,10 +16,14 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  
   return (
     <html lang="en">
-      <body className={`${inter.className} dark`}>{children}
-      <Footer /> 
+      <body className={`${inter.className} dark`}>
+        <AuthProvider>
+            {children}
+            <Footer /> 
+          </AuthProvider>
       </body>
     </html>
   );
