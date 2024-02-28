d26d5f7e72416023aacb3807ee6c97bf126e0532
Lusca-rcg <lusca.rcg@gmail.com>
2024-02-28 16:47:08 -0300

    feat: create booking page

--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -1,10 +1,15 @@
 "use client"
 
 import { Button } from "@/app/_components/ui/button";
+import { Calendar } from "@/app/_components/ui/calendar";
 import { Card, CardContent } from "@/app/_components/ui/card";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/app/_components/ui/sheet";
 import { Service } from "@prisma/client";
+import { pt, ptBR } from "date-fns/locale";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
+import { useMemo, useState } from "react";
+import { generateDayTimeList } from "../_helpers/hours";
 
 interface ServiceItemProps {
     service: Service;
@@ -12,11 +17,28 @@ interface ServiceItemProps {
 }
 
 const ServiceItem = ({ service, isAuthenticated }: ServiceItemProps) => {
+
+    const [date, setDate] = useState<Date | undefined>(new Date())
+    const [hour, setHour] = useState<string | undefined>()
+
+    const handleDateClick = (date: Date | undefined) => {
+        setDate(date);
+        setHour(undefined)
+    }
+
+    const handleHourClick = (time : string) => {
+        setHour(time)
+    }
+
     const handleBookingClick = () => {
         if(!isAuthenticated) {
             return signIn('google');
         }
-    }
+    };
+
+    const timeList = useMemo(() => {
+        return date ? generateDayTimeList(date) : [];
+    }, [date]);
 
     return (
         <Card>
@@ -37,7 +59,79 @@ const ServiceItem = ({ service, isAuthenticated }: ServiceItemProps) => {
                                     currency: "BRL",
                                 }).format(Number(service.price)) 
                             }</p>
-                            <Button variant="secondary" onClick={handleBookingClick}>Agendar</Button>
+
+                            <Sheet>
+
+                              <SheetTrigger asChild>
+                                <Button variant="secondary" onClick={handleBookingClick}>Agendar</Button>
+
+                              </SheetTrigger>
+                              
+                              <SheetContent className="p-0">
+                                <SheetHeader className="text-left px-5 py-6 border-b border-solid border-secondary">
+                                    <SheetTitle>Fazer Reserva</SheetTitle>
+                                </SheetHeader>
+
+                                <div className="py-6">
+                                <Calendar
+                                    mode="single"
+                                    selected={date}
+                                    locale={ptBR}
+                                    onSelect={handleDateClick}
+                                    fromDate={new Date()}
+                                    styles={{
+                                        head_cell: {
+                                            width: "52px",
+                                            textTransform: "capitalize",
+                                        },
+                                        cell: {
+                                            width: "100%",
+                                        },
+                                        button: {
+                                            width: "100%",
+                                        },
+                                        nav_button_previous: {
+                                            width: "32px",
+                                            height: "32px",
+                                        },
+                                        nav_button_next: {
+                                            width: "32px",
+                                            height: "32px",
+                                        },
+                                        caption: {
+                                            textTransform: "capitalize",
+                                        },
+
+                                    }}
+                                />
+                                </div>
+
+                                {date && (
+                                    <div className="flex gap-3 overflow-x-auto py-6 px-5 border-y border-solid border-secondary [&::-webkit-scrollbar]:hidden">
+                                        {timeList.map((time) => (
+                                            <Button onClick={() => handleHourClick(time)} variant={hour === time ? "default" : "outline"} className="rounded-full" key={time}>{time}</Button>
+                                        ))}
+
+                                    </div>
+                                )}
+
+                                <div className="py-6 px-5 border-t border-solid border-secondary">
+                                    <Card>
+                                        <CardContent className="p-3">
+                                            <div className="flex justify-between">
+                                                
+                                            </div>
+                                        </CardContent>
+                                    </Card>
+
+
+                                </div>
+                    
+
+
+                              </SheetContent>
+
+                            </Sheet>
 
                         </div>
                     
