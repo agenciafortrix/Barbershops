e3bb0982aed7c62aba1be2b1669131fe0df5eeda
Lusca-rcg <lusca.rcg@gmail.com>
2024-03-12 09:27:34 -0300

    style: update a new logo

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
       <Card>
         <CardContent className="p-5 justify-between items-center flex flex-row">
           <Link href="/">
-            <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
+            <Image src="/logo.png" alt="FSW Barber" height={20} width={220} />
           </Link>
           <Sheet>
             <SheetTrigger asChild>
